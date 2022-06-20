1dbf5faf525af67ff2bd84c7551dbfcc40269275
Deiyox <98240432+AnthoEnge@users.noreply.github.com>
2022-06-20 19:54:23 +0200

    add Competence

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ function ChangeMotH1() {
     let listeMot = ["Web" ,  "Front-End" ];
     let changeMot = document.querySelector("#changeMot");
     changeMot.innerHTML = Math.random() > 0.5 ? listeMot[0] : listeMot[1];
-        
 }setInterval(ChangeMotH1 ,3000);
+
+function Day() {
+    let span = document.querySelector(".orange");
+    document.querySelector(".day").addEventListener("click" , function() {
+        let body = document.querySelector("body");
+        body.classList.toggle("bgDay");
+    })
+}Day();
